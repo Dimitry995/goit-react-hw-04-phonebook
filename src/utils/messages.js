3d034db5ee3b6cd72ab2bfc0ced7b3d959b022ad
Dimitry995 <98602983+Dimitry995@users.noreply.github.com>
2023-03-12 20:29:21 +0200

    Update messages.js

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,4 +2,4 @@ export const messageForName =
   "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, d'Artagnan";
 
 export const messageForNumber =
-  'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +';
+  'Phone number must be digits';
